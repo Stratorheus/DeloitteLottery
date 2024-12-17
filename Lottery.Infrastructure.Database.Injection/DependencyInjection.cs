2d55556eed20fd9f72184dfdc39f3096a73766fc
Martin Skořupa <martin.skorupa.personal@outlook.com>
2024-12-17 20:57:17 +0100

    Infrastructure IoC

---
+++ Lottery.Infrastructure.Database.Injection/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Lottery.Domain.Abstract.Infrastructure.Database;
+using Lottery.Domain.Configuration;
+using Lottery.Infrastructure.Database.Ef.Context;
+using Lottery.Infrastructure.Database.Repositories;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lottery.Infrastructure.Database.Injection
+{
+    public static class DependencyInjection
+    {
+        public static void AddDatabase(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<LotteryDbContext>(opt => opt.UseSqlServer(config.GetConnectionString(ConfigKeys.DbConnectionString)));
+            services.AddScoped<IDrawHistoryRepository, DrawHistoryRepository>( );
+        }
+    }
+}
